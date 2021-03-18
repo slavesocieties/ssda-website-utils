78519dedaac26caafbec39e6a854d9a80b6f8ee2
d-genk <daniel.n.genkins@vanderbilt.edu>
2021-03-18 17:35:43 -0500

    built s3 scraper

--- ssda-website-v2/_nbdev.py
+++ ssda-website-v2/_nbdev.py
@@ -3,9 +3,12 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"build_volume_manifest": "manifest_generation.ipynb",
-         "write_server": "manifest_generation.ipynb"}
+         "write_server": "manifest_generation.ipynb",
+         "scrape_bucket": "s3_scrape.ipynb",
+         "ssda_volume_xml_to_dict": "s3_scrape.ipynb"}
 
-modules = ["manifest_gen.py"]
+modules = ["manifest_gen.py",
+           "s3_scrape.py"]
 
 doc_url = "https://slavesocieties.github.io/ssda-website-v2/"
 
